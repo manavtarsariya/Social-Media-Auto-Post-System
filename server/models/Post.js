6b6921fcfd983f9cd2c95df34a7db5941581bc9a
manavtarsariya <manavatarsariya@gmail.com>
2025-10-01 18:10:18 +0530

    schema added

---
+++ server/models/Post.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String
+    },
+    imageUrl: {
+        type: String
+    },
+    hashtags: [String],
+    scheduleTime: {
+        type: Date
+    },  // when to auto-post
+    status: {
+        type: String,
+        enum: ["pending", "scheduled", "posted", "failed"],
+        default: "pending"
+    },
+    aiCaption: {
+        type: String
+    }, // optional AI generated caption/hashtags
+
+},{timestamps: true});
+
+
+export const Post = mongoose.model("Post", PostSchema);      
+export default Post;
