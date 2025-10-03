3bbcd30edbccde8207fb97ee48ef69ccfde280ee
manavtarsariya <manavatarsariya@gmail.com>
2025-10-03 14:33:36 +0530

    node_cron setup

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 import postRoutes from './routes/postRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import sheduler from './schedulers/postScheduler.js';
+// import sheduler from './schedulers/postScheduler.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,7 +23,7 @@ app.use('/api/posts',postRoutes);
 
 app.listen(PORT, () => {
     connectDB();
-    sheduler.start()
+    // sheduler.start()
     // sheduler.stop()
     console.log(`Server is running on port ${PORT}`);
 });
